9e200a627b4b5361abf00e7176e30dacf7d23637
Aberosha <isabellakaigorodova@gmail.com>
2024-12-10 09:30:01 +0200

    Add files via upload

---
+++ untitled4/src/Main.java
@@ -0,0 +1,36 @@
+package stunda2;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        
+    }
+    public static void sum(int a, int b){
+        int res = a+b;
+        System.out.println("Summa ir "+res);
+    }
+
+}
+public void batter(){
+    System.out.println("Mix the dry ingredients.");
+    System.out.println("Cream the butter and sugar.");
+    System.out.println("Beat in the eggs.");
+    System.out.println("Stir in the dry ingredients.");
+}
+
+public void bake(){
+    System.out.println("Set the oven temperature.");
+    System.out.println("Set the timer.");
+    System.out.println("Place a batch of cookies into the oven.");
+    System.out.println("Allow the cookies to bake.");
+    System.out.println("Set the oven temperature.");
+    System.out.println("Set the timer.");
+    System.out.println("Place a batch of cookies into the oven.");
+    System.out.println("Allow the cookies to bake.");
+}
+
+public void frosting(){
+    System.out.println("Mix ingredients for frosting.");
+    System.out.println("Spread frosting and sprinkles.");
+}
